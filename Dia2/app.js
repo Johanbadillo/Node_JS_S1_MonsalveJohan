f25978fdb9eaea5332404769e0de6f7112be47dd
johan <johanbadillomj@gmail.com>
2025-11-07 09:01:38 -0500

    feat: :sparkles: add day two

---
+++ Dia2/app.js
@@ -0,0 +1,6 @@
+// console.log("Mundoooooooooooo");
+const prompt = require('prompt-sync')({ sigint: true });
+
+const nombre = prompt("Cual es tu nombre??");
+
+console.log("Tu nombre es: " +nombre);
